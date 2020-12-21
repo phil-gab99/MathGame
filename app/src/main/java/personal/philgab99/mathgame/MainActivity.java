87dea570c9d080aaba6000aeeb1551a138f8b63b
phil-gab99 <pgabriel999@hotmail.com>
2020-12-21 18:54:10 -0500

    Auto-generated files

---
+++ app/src/main/java/personal/philgab99/mathgame/MainActivity.java
@@ -0,0 +1,13 @@
+package personal.philgab99.mathgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

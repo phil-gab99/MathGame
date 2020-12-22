8e95914292efebe6061ffbf302ba0d5493b9ef2c
phil-gab99 <pgabriel999@hotmail.com>
2020-12-22 04:12:17 -0500

    App is working!

--- app/src/main/java/personal/phil_gab99/mathgame/MainActivity.java
+++ app/src/main/java/personal/phil_gab99/mathgame/MainActivity.java
@@ -1,64 +1,85 @@
 package personal.phil_gab99.mathgame;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.Random;
 
-/**
-* The class MainActivity
-*
-* @author Philippe Gabriel
-* @version 1.0 2020-12-21
-***/
+ /**
+ * The class MainActivity
+ *
+ * @author Philippe Gabriel
+ * @version 1.4 2020-12-21
+ ***/
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView num1;
-    TextView num2;
+    private TextView num1;
+    private TextView num2;
 
-    /**
-    * The method onCreate
-    * 
-    * @param savedInstanceState Bundle
-    * @see android.app.Activity
-    ***/
+    private TextView good;
+    private TextView bad;
+
+     /**
+     * The method onCreate
+     *
+     * @param savedInstanceState Bundle
+     * @see android.app.Activity
+     ***/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        int val1 = 12;
+
         num1 = findViewById(R.id.Number1);
-        num1.setText(val1);
-        
-        int val2 = 64;
         num2 = findViewById(R.id.Number2);
-        num2.setText(val2);
+
+        good = findViewById(R.id.good);
+        bad = findViewById(R.id.bad);
+
+        generateNumbers();
     }
-    
-    /**
-    * The method onSubmitOnClick
-    * 
-    * @param view
-    ***/
-    
-    public void onSubmitOnClick(View view) {
-        
+
+    public void generateNumbers() {
+
+        Random rand = new Random();
+
+        int val1 = rand.nextInt(100);
+        int val2 = rand.nextInt(100);
+
+        num1.setText("" + val1);
+        num2.setText("" + val2);
+    }
+
+     /**
+     * The method onSubmitClick
+     *
+     * @param view View
+     ***/
+
+    public void onSubmitClick(View view) {
+
         TextView answer = findViewById(R.id.Answer);
         EditText attempt = findViewById(R.id.Attempt);
-        
+
         int userAnswer = Integer.parseInt(attempt.getText().toString());
-        
+
         int val1 = Integer.parseInt(num1.getText().toString());
         int val2 = Integer.parseInt(num2.getText().toString());
-        
+
         if (userAnswer == (val1 + val2)) {
-            
-            Answer.setText("Correct!");
+
+            answer.setText("Correct!");
+            good.setText("" + (Integer.parseInt(good.getText().toString()) + 1));
         } else {
-            
-            Answer.setText("Wrong! The correct answer is " + (val1 + val2));
+
+            answer.setText("Wrong! The correct answer is " + (val1 + val2));
+            bad.setText("" + (Integer.parseInt(bad.getText().toString()) + 1));
         }
+
+        generateNumbers();
     }
 }
